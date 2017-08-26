c5d052bc41b05186c903dd574381a71df66b5a38
Kate Gleeson <allbecauseyoutoldmeso@yahoo.co.uk>
2017-08-26 15:06:34 +0100

    where im at

--- app/models/text_alert.rb
+++ app/models/text_alert.rb
@@ -9,11 +9,12 @@ class TextAlert
     @message_text = message_text
     @twilio_sid = ENV['TWILIO_SID']
     @twilio_token = ENV['TWILIO_TOKEN']
-    @twilio_number = ENV['TWILIO_NUMBER'
+    @twilio_number = ENV['TWILIO_NUMBER']
   end
 
   def send_texts
     client = Twilio::REST::Client.new(twilio_sid, twilio_token)
+    # User.where.not(id: message_user.id).pluck(:mobile).each do |mobile|
     User.pluck(:mobile).each do |mobile|
       client.api.account.messages.create(
         from: twilio_number,
@@ -24,7 +25,7 @@ class TextAlert
   end
 
   def text_content
-    "#{message_user} posted a new message on we_three: #{message_text}\nto reply go to https://we-three.herokuapp.com."
+    "#{message_user.email} posted a new message on we_three: #{message_text}\nto reply go to https://we-three.herokuapp.com"
   end
 
 end
